06cd270cca716aaf932e3cd2811ae1fdd56abc7e
Neil Power <neil.power@procurify.com>
2021-12-12 23:03:21 -0800

    Day 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = input.map { it.toInt() }.fold(Pair(-1, 0)) { pair, value ->
+        Pair(value, if (value > pair.first) pair.second + 1 else pair.second)
+    }.second - 1
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val intInput = input.map { it.toInt() }
+        return intInput
+            .mapIndexedNotNull { index, _ -> if (index > 1) intInput[index] + intInput[index - 1] + intInput[index - 2] else null }
+            .fold(Pair(0, -1)) { pair, value ->
+                Pair(
+                    value, if (value > pair.first) pair.second + 1 else pair.second
+                )
+            }.second
     }
 
-    // test if implementation meets criteria from the description, like:
+// test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    check(part1(testInput) == 7)
+    println(part2(testInput))
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
